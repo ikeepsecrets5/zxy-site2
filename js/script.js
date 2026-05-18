f65c6d4a460cb054821248a010f9ea33ec55e1d2
ikeepsecrets5 <euamoo67resenha@gmail.com>
2026-05-18 19:15:20 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("VOID RNG loaded");
+
+document.querySelectorAll('.card,.stat').forEach((el)=>{
+  el.addEventListener('mousemove',(e)=>{
+    const r=el.getBoundingClientRect();
+    el.style.transform=`translateY(-6px) rotateX(${-(e.clientY-r.top-r.height/2)/35}deg) rotateY(${(e.clientX-r.left-r.width/2)/35}deg)`;
+  });
+  el.addEventListener('mouseleave',()=>{el.style.transform='';});
+});
